ab38046502820e2692b6b158643881c252c8617c
Prashant Babu <www.babuprashant731@gmail.com>
2023-10-13 12:53:06 +0530

    tried

--- main/react/new-assign-app/src/App.js
+++ main/react/new-assign-app/src/App.js
@@ -3,6 +3,7 @@ import Rishabh from './components/Rishabh'
 import './App.css';
 import { useAuth0 } from "@auth0/auth0-react";
 
+
 function App() {
   const { user, isAuthenticated  } = useAuth0();
   return (
